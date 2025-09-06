2cdf7c722825a4bdfd60d79509b17c5187d646f5
lesterguscis <lesterguscis@gmail.com>
2025-09-06 17:34:46 +0200

    Adaptar app para Render

--- app.py
+++ app.py
@@ -80,4 +80,5 @@ def submit():
     return "✅ Registro guardado con éxito en Excel."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
